b1df229dcf2d4a434fbe712300d316dbaa84f363
Barsch2006 <cchr98756@gmail.com>
2023-02-26 21:23:06 +0100

    update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*
+Imports
+*/
+require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
@@ -5,90 +9,102 @@ const SQLiteStore = require('connect-sqlite3')(session);
 const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 
-// Initialisierung der Datenbankverbindung
-const db = new sqlite3.Database('./sqlite.sqlite');
-db.serialize(function () {
-  db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)");
-  bcrypt.hash('testpassword', 10, (err, hash) => {
-    db.run("INSERT INTO users (username, password) VALUES (?, ?)", ["testuser", hash], (err) => {
-      if (err) {
-        console.log('user exists')
-      }
-    });
-  });
+/*
+Database
+*/
+const db = new sqlite3.Database(process.env.DB ?? './db.sqlite');
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+    )`);
 });
 
-// Initialisierung des Express-Servers
+bcrypt.hash('testpassword', 10, (err, hash) => {
+    db.run("INSERT INTO users (username, password) VALUES (?, ?)", ["tester", hash], (err) => {
+        if (err) {
+            console.log('test-user all ready exists')
+        }
+    });
+});
+
+/*
+Express Server
+*/
 const app = express();
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded())
-
 // Konfiguration der Session- und Passport-Authentifizierung
 app.use(session({
-  store: new SQLiteStore({
-    db: './sqlite.sqlite',
-    table: 'sessions',
-    ttl: 86400 // Session-TTL von einem Tag (in Sekunden)
-  }),
-  secret: 'geheimnis',
-  resave: false,
-  saveUninitialized: false
+    store: new SQLiteStore({
+        db: process.env.DB ?? './db.sqlite',
+        table: 'sessions',
+        ttl: 86400 // Session-TTL von einem Tag (in Sekunden)
+    }),
+    secret: process.env.SESSION_SECRET ?? undefined,
+    resave: false,
+    saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+/*
+Passport.js Konfiguration
+*/
 // Konfiguration der Passport-Strategie (hier wird die lokale Strategie verwendet)
 const LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy(
-  function (username, password, done) {
-    db.get("SELECT * FROM users WHERE username = ?", username, function (err, row) {
-      if (err) { return done(err); }
-      if (!row) { return done(null, false); }
-      bcrypt.compare(password, row.password, function (err, res) {
-        if (res === true) {
-          return done(null, row);
-        } else {
-          return done(null, false);
-        }
-      });
-    });
-  }
+    (username, password, done) => {
+        db.get("SELECT * FROM users WHERE username = ?", username, (err, row) => {
+            if (err) { return done(err); }
+            if (!row) { return done(null, false); }
+            bcrypt.compare(password, row.password, (err, res) => {
+                if (res === true) {
+                    return done(null, row);
+                } else {
+                    return done(null, false);
+                }
+            });
+        });
+    }
 ));
-
 // Konfiguration der Passport-Serialisierung (hier wird die Benutzer-ID serialisiert)
-passport.serializeUser(function (user, done) {
-  done(null, user.id);
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    db.get("SELECT * FROM users WHERE id = ?", id, (err, row) => {
+        if (err) { return done(err); }
+        done(null, row);
+    });
 });
 
-passport.deserializeUser(function (id, done) {
-  db.get("SELECT * FROM users WHERE id = ?", id, function (err, row) {
-    if (err) { return done(err); }
-    done(null, row);
-  });
-});
-
+/*
+Express Router Listener
+*/
 // Routen-Handler für den Login-Vorgang
 app.post('/login',
-  passport.authenticate('local'),
-  function (req, res) {
-    res.cookie('sessionID', req.sessionID);
-    res.send('Login erfolgreich');
-  }
+    passport.authenticate('local'),
+    function (req, res) {
+        res.cookie('sessionID', req.sessionID);
+        res.send('Login erfolgreich');
+    }
 );
 
 // Routen-Handler für den Logout-Vorgang
 app.get('/logout', function (req, res) {
-  req.logout((err) => {
-    if (err) {
-      console.error(err)
-    } else {
-      req.session.destroy();
-      res.clearCookie('sessionID');
-      res.send('Logout erfolgreich');
+    req.logout((err) => {
+        if (err) {
+            console.error(err)
+        } else {
+            req.session.destroy();
+            res.clearCookie('sessionID');
+            res.send('Logout erfolgreich');
 
-    }
-  });
+        }
+    });
 });
 
 // Routen-Handler für geschützte Ressourcen
@@ -97,14 +113,19 @@ app.get('/geschuetzt',
     if (req.isAuthenticated()) {
       res.send('Zugriff auf geschützte Ressource gewährt');
     } else {
-      res.redirect('/login');
+      res.redirect('/');
     }
   }
 );
 
-app.use(express.static('C:/Users/Sabine/Desktop/Login/frontend'))
+/*
+Express Static
+*/
+app.use(express.static(process.env.STATIC));
 
-// Starten des Servers
-app.listen(3000, function () {
-  console.log('Server gestartet auf Port 3000');
+/*
+Express Listen
+*/
+app.listen(process.env.PORT, () => {
+    console.log(`Server gestartet auf Port ${process.env.PORT}`);
 });
