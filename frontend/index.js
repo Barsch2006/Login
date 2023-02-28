7ad68f7b287a426a900883c74239f1dff773cd5d
Barsch2006 <cchr98756@gmail.com>
2023-02-28 12:05:46 +0100

    muster finished, login finished, cookies finished

--- frontend/index.js
+++ frontend/index.js
@@ -6,3 +6,16 @@ function checkCookies() {
         window.location.href = 'cookie.html'
     }
 }
+
+function checkURI() {
+    if (containsWrongParam(new URL(window.location))) {
+        document.querySelector('#wrong').style.display = "block";
+    } else {
+        document.querySelector('#wrong').style.display = "none";
+    }
+}
+
+function containsWrongParam(url) {
+    const params = new URLSearchParams(url.search);
+    return params.has('wrong') && params.get('wrong') === 'true';
+}
